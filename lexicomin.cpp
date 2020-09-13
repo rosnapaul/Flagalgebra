857d1c749043a77a1bdf453eb8b5f42ae897130a
Thejus Paul <thejuspaul@pm.me>
2020-09-13 23:50:07 +0530

    refactor: removed unwanted spaces

--- lexicomin.cpp
+++ lexicomin.cpp
@@ -3,64 +3,44 @@
 #include<string>
 #include<vector>
 #include<math.h>
-
 using namespace std;
 
-
-
-bool islexicomin (int min[][m_vertices], int new_mat[][m_vertices], int m_vertices)
-{
-	for( int i=0; i< m_vertices; i++)
-	  {
-		for(int j=0; j<m_vertices-1; j++)
-		  {
-			  if ( new_mat[i][j] < min[i][j])
-				  return true;
-        	}
-   	  }
-	
-	
-	
-	
-	
-	
+bool islexicomin(new min[][m_vertices], new int new_mat[][m_vertices], int m_vertices) {
+	for(int i=0; i< m_vertices; i++) {
+		for(int j=0; j<m_vertices-1; j++) {
+			if (new_mat[i][j] < min[i][j]) {
+				return true;
+			}
+		}
+	}
 }
 
-
-int main()
-{
-	int m_verticesi,j,k,l,m;
-	cout<<"Enter the number of vertices"<<endl;
+int main() {
+	int m_vertices,i,j,k,l,m;
+	cout<<"Enter the number of vertices: ";
 	cin>>m_vertices;
-	int min[m_vertices][m_vertices ], new_mat[m_vertices][m_vertices];
+	int min[m_vertices][m_vertices], new_mat[m_vertices][m_vertices];
 	int v[m_vertices];
-	
-	
 	// collecting the rotation sysytem
-	cout<<"Enter the rotation system"<<endl;
-	for(i=0; i< m_vertices; i++)
-	  {
-		for(j=0; j<m_vertices-1; j++)
-		  {
+	cout<<"Enter the rotation system: "<<endl;
+	for(i=0; i< m_vertices; i++) {
+		for(j=0; j<m_vertices-1; j++) {
 			cin>>new_mat[i][j];
-        	}
+        }
    	}
-	
-	cout<<"Enter the minimun system"<<endl;
-	for(i=0; i< m_vertices; i++)
-	  {
-		for(j=0; j<m_vertices-1; j++)
-		  {
+	// collecting the minimum system
+	cout<<"Enter the minimum system: "<<endl;
+	for(i=0; i<m_vertices; i++) {
+		for(j=0; j<m_vertices-1; j++) {
 			cin>>min[i][j];
-        	}
+        }
    	}
 	
-	if (islexicomin (min, new_mat, m_vertices))
-	{
-		cout<< " minimum has changed"<<endl;
+	if(islexicomin(min, new_mat, m_vertices)) {
+		cout<<"minimum has changed"<<endl;
 	}
-	
-	else
-		cout<<"mini remains same"<<endl;
-		
+	else {
+		cout<<"minimum remains same"<<endl;
+	}
+	return 0;
 }	
