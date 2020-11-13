fe6d63a1ed86758d4a1730a662a95cb8c47ea578
Thejus Paul <thejuspaul@pm.me>
2020-11-13 22:52:40 +0530

    Added Comments to read_matrix.cpp

--- read_matrix.cpp
+++ read_matrix.cpp
@@ -2,10 +2,12 @@
 #include"read_matrix.h"
 
 int main(void) {
-    collection_of_matrices matrices = get_matrices();
-    for(short i = 0; i < matrices.size(); i++) {
-        for(short j = 0; j < MATRIX_ROW_SIZE; j++) {
-            for(short k = 0; k < MATRIX_COLUMN_SIZE; k++)
+    int i,j,k; // Variables Needed For Loop
+    collection_of_matrices matrices = get_matrices(); // `matrices` Contains All 262 Matrices.
+
+    for(i = 0; i < matrices.size(); i++) { // matrices.size() gives 261
+        for(j = 0; j < MATRIX_ROW_SIZE; j++) { // MATRIX_ROW_SIZE is set as 6
+            for(k = 0; k < MATRIX_COLUMN_SIZE; k++) // MATRIX_COLUMN_SIZE is set as 5
                 cout << matrices[i][j][k] << " ";
             cout << endl;
         }
