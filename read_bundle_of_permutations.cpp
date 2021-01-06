e123a824e32c1c902f785b5983f7ad032d40cfbf
Thejus Paul <thejuspaul@pm.me>
2021-01-06 23:47:41 +0530

    feat(*): Outputs 'flag.txt'

--- read_bundle_of_permutations.cpp
+++ read_bundle_of_permutations.cpp
@@ -10,14 +10,16 @@ typedef vector<vector<vector<int>>> collection_of_matrices;
 const unsigned short NO_OF_MATRICES = 6;
 const unsigned short MATRIX_ROW_SIZE = 6;
 const unsigned short MATRIX_COLUMN_SIZE = 5;
+collection_of_matrices lexicographic_min;
 
-collection_of_matrices get_matrices(void) {
+collection_of_matrices get_matrices(int fileNumber) {
     collection_of_matrices matrices;
     unsigned short i,j,k,temp; // Variables for Looping or Temporary Purposes
     unsigned char temp_char;
     ifstream infile;
     string file_dir = "./bundle_of_permutations/";
-    infile.open(file_dir+"1.txt"); // To read from bundle_of_permutations folder
+    string fileNum = to_string(fileNumber);
+    infile.open(file_dir+fileNum+".txt"); // To read from bundle_of_permutations folder
     // To Read Data from File and Insert into `matricies`
     for(i = 0; i < NO_OF_MATRICES; i++) {
         collection_of_rows matrix;
@@ -35,23 +37,9 @@ collection_of_matrices get_matrices(void) {
     infile.close();
     return matrices;
 }
-void print(void) {
-    unsigned short i,j,k;
-    collection_of_matrices matrices = get_matrices();
-    // To Print the Read Matrix
-    for(i = 0; i < matrices.size(); i++) {
-        cout << "Matrix " << i+1 << ":" << endl;
-        for(j = 0; j < MATRIX_ROW_SIZE; j++) {
-            for(k = 0; k < MATRIX_COLUMN_SIZE; k++)
-                cout << matrices[i][j][k] << " ";
-            cout << endl;
-        }
-        cout << endl;
-    }
-}
 
 void lexicomini_of(vector<vector<vector<int>>> compmat, vector<vector<int>> &min) {
-	bool flag = false;
+    bool flag = false;
 	for(int k = 0; k < compmat.size(); k++) {
 		flag = false;
 		for(int i = 0; i < compmat[k].size(); i++) {  
@@ -60,10 +48,17 @@ void lexicomini_of(vector<vector<vector<int>>> compmat, vector<vector<int>> &min
 					flag = true;
 			 		break;
 				}
-				if(compmat[k][i][j] < min[i][j]) {          
-					for(int i = 0; i < compmat[k].size(); i++)
-						for(int j=0;j < compmat[k][i].size(); j++)
-							min[i][j] = compmat[k][i][j];
+				if(compmat[k][i][j] < min[i][j]) { 
+                    collection_of_rows matrix;         
+					for(int i = 0; i < compmat[k].size(); i++) {
+                        row temp_row;
+                        for(int j=0;j < compmat[k][i].size(); j++) {
+                            min[i][j] = compmat[k][i][j];
+                            temp_row.push_back(min[i][j]);
+                        }
+                        matrix.push_back(temp_row);
+                    }
+                    lexicographic_min.push_back(matrix);
 					break;
 				}
 			}
@@ -73,8 +68,27 @@ void lexicomini_of(vector<vector<vector<int>>> compmat, vector<vector<int>> &min
 	}
 }
 
+void dump_lexicomgraphic_min(void) {
+    unsigned short i,j,k;
+    ofstream outfile;
+    outfile.open("./flags.txt", ofstream::out);
+    if(!outfile.good())
+        cout<<"failed opening file "<<"./flags.txt"<<endl;
+    for(i = 0; i < lexicographic_min.size(); i++) {
+        for(j = 0; j < MATRIX_ROW_SIZE; j++) {
+            for(k = 0; k < MATRIX_COLUMN_SIZE; k++)
+                outfile << lexicographic_min[i][j][k] << " ";
+            outfile << endl;
+        }
+        outfile << endl;
+    }
+}
+
 int main(void) {
-    collection_of_matrices matrices = get_matrices();
-    lexicomini_of(matrices, matrices[0]);
+    for(int i = 1; i <= 120; i++) {
+        collection_of_matrices matrices = get_matrices(i);
+        lexicomini_of(matrices, matrices[0]);
+    }
+    dump_lexicomgraphic_min();
     return 0;
 }
