56f59d35ad469088e55551862044dd60d9058ac0
Thejus Paul <thejuspaul@pm.me>
2021-03-17 18:55:47 +0530

    fix(): Minor Changes

--- read_matrix.h
+++ read_matrix.h
@@ -10,16 +10,15 @@ typedef vector<int> row;
 typedef vector<vector<int>> collection_of_rows;
 typedef vector<vector<vector<int>>> collection_of_matrices;
 
-const unsigned short NO_OF_MATRICES = 262;
 const unsigned short MATRIX_ROW_SIZE = 6;
 const unsigned short MATRIX_COLUMN_SIZE = 5;
 
-inline collection_of_matrices get_matrices(void) {
+inline collection_of_matrices get_matrices(string filename,unsigned short NO_OF_MATRICES) {
     collection_of_matrices matrices;
     unsigned short i,j,k,temp; // Variables for Looping or Temporary Purposes
     unsigned char temp_char;
     ifstream infile;
-    infile.open("rotationinmyform.txt"); // To read "rotationinmyform.txt" file
+    infile.open(filename); // To read the file
     // To Read Data from File and Insert into `matricies`
     for(i = 0; i < NO_OF_MATRICES; i++) {
         collection_of_rows matrix;
@@ -38,9 +37,9 @@ inline collection_of_matrices get_matrices(void) {
     return matrices;
 }
 
-inline void print(void) {
+inline void print_all(void) {
     unsigned short i,j,k;
-    collection_of_matrices matrices = get_matrices();
+    collection_of_matrices matrices = get_matrices("flags.txt",120);
     // To Print the Read Matrix
     for(i = 0; i < matrices.size(); i++) {
         cout << "Matrix " << i+1 << ":" << endl;
@@ -50,6 +49,19 @@ inline void print(void) {
             cout << endl;
         }
         cout << endl;
+    }
+}
+
+inline void print(int index) {
+    unsigned short i,j,k;
+    collection_of_matrices matrices = get_matrices("flags.txt",120);
+    // To Print the Read Matrix
+    cout << "Matrix " << index << ":" << endl;
+    for(j = 0; j < MATRIX_ROW_SIZE; j++) {
+        for(k = 0; k < MATRIX_COLUMN_SIZE; k++) {
+            cout << matrices[index][j][k] << " ";
+        }  
+    cout << endl;
     }
 }
 
