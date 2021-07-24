099b1a44a1700dc01fb062e3e2b47dc196fac9f4
Thejus Paul <32426070+Thejus-Paul@users.noreply.github.com>
2021-07-24 18:01:13 +0530

    fix: traverse through all input matrices

--- main.cpp
+++ main.cpp
@@ -17,10 +17,7 @@ typedef vector<vector<int> > collection_of_rows;
 typedef vector<vector<vector<int> > > collection_of_matrices;
 typedef vector<vector<vector <vector<int> > > > collection_of_collection_of_matrices;
 
-collection_of_collection_of_matrices classification_of_permutation, matrices_in_type_matrix;
-collection_of_matrices matrices,lexicographic_min,pruned_lexico_min;
-collection_of_rows matrix;
-row temp_row,duplicate_entries;
+collection_of_collection_of_matrices matrices_in_type_matrix;
 
 int search_in_array(int v[], int s, int l) {
 	for(int i = 0; i < l; i++) {
@@ -90,181 +87,7 @@ int main(void) {
 		for(j = 0; j < (m_vertices-1); j++)
 			cin>>rot_sys[i][j]; */
 	int v[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-	//int rot_sys[m_vertices][m_vertices-1] = {{2,3,4,5,6}, {1,3,4,5,6}, {1,2,4,5,6}, {1,2,3,5,6}, {1,2,6,3,4}, {1,2,5,3,4}};
-	// To read from the `input.txt` file (reads only one matrix)
-	collection_of_matrices tmp = get_matrices("input.txt",165,m_vertices,m_vertices-1);
-	int rot_sys[m_vertices][m_vertices-1];
-	for(i = 1; i < 2; i++)
-		for(j = 0; j < tmp[i].size(); j++)
-			for(k = 0; k < tmp[i][j].size(); k++)
-				rot_sys[j][k] = tmp[i][j][k];
-
-	// To print the `rot_sys` matrix.
-/* 	for(i = 0; i < m_vertices; i++) {
-		for(j = 0; j < (m_vertices-1); j++)
-			cout<<rot_sys[i][j]<<" ";
-		cout<<endl;
-	} */
-
-	int new_clock[m_vertices][m_vertices-1];
-	memset( new_clock, 0, m_vertices*(m_vertices-1)*sizeof(int) );
-
-	std::sort(v, v+m_vertices);
-
-	int final_bundle_size = factorial(m_vertices - type_size);
-
-
-	int num_bundle = 1, bundle_size = 1;
-
-	do {
-		/* for(i = 0; i < m_vertices; i++) {
-				cout<<v[i]<<" ";
-		}
-		cout<<endl<<endl; */
-
-		dummy = search_in_array(rot_sys[v[0]-1], v[1], (m_vertices - 1));
-		for(i = 0; i < (m_vertices - 1); i++) {
-			flag = false;
-			new_clock[0][i] = 1 + search_in_array(v, rot_sys[v[0]-1][(dummy+i) % (m_vertices-1)], m_vertices);
-		}
-		for(int h = 1; h < m_vertices; h++) { // finding the relabeled vertices and rotating them such that least element comes first.
-			flag = false;
-			dummy = search_in_array(rot_sys[v[h]-1], v[0], (m_vertices-1)); // looking for the clockwise direction
-			for(i = 0; i < (m_vertices - 1); i++) {
-				flag = false;
-				new_clock[h][i] = 1 + search_in_array(v, rot_sys[v[h]-1][(dummy + i) % (m_vertices - 1)], m_vertices);
-			}
-		}
-
-		for(i = 0; i < m_vertices; i++) {
-			for(j = 0; j < (m_vertices - 1); j++) {
-				//cout<<new_clock[i][j]<<" ";
-				temp_row.push_back(new_clock[i][j]);
-			}
-			matrix.push_back(temp_row);
-			temp_row.clear();
-		}
-		matrices.push_back(matrix);
-		matrix.clear();
-
-		if(bundle_size == final_bundle_size) {
-			bundle_size = 1;
-			++num_bundle;
-			classification_of_permutation.push_back(matrices);
-			matrices.clear();
-		} else {
-			++bundle_size;
-		}
-	}while(std::next_permutation(v,v+m_vertices));
-
-	// To print `classfication_of_permutation`
-	/* cout<<"Classification of Permutation:"<<endl;
-	for(int i=0; i<classification_of_permutation.size(); i++) {
-		cout<<"The bundle size "<<i+1<<": "<<endl;
-		for(int j=0; j<classification_of_permutation[i].size(); j++) {
-			cout<<"Matrix "<<j+1<<": "<<endl;
-			for(int k=0; k<classification_of_permutation[i][j].size(); k++) {
-				for(int l=0; l<classification_of_permutation[i][j][k].size(); l++) {
-					cout<<classification_of_permutation[i][j][k][l]<<" ";
-				}
-				cout<<endl;
-			}
-			cout<<endl;
-		}
-	} */
-
-	for(int i=0; i<classification_of_permutation.size(); i++) {
-		if(classification_of_permutation[i].size() > 1) {
-			lexicomini_of(classification_of_permutation[i], classification_of_permutation[i][0]);
-			lexicographic_min.push_back(classification_of_permutation[i][0]);
-		} else if(classification_of_permutation[i].size() == 1) {
-			lexicographic_min.push_back(classification_of_permutation[i][0]);
-		}
-
-	}
-
-	// To print `lexicographic_min`
-	/*
-	cout<<"Lexicographic Minimum:"<<endl;
-	for(int i=0; i<lexicographic_min.size(); i++) {
-		cout<<"Matrix "<<i+1<<": "<<endl;
-		for(int j=0; j<lexicographic_min[i].size(); j++) {
-			for(int k=0; k<lexicographic_min[i][j].size(); k++) {
-				cout<<lexicographic_min[i][j][k]<<" ";
-			}
-			cout<<endl;
-		}
-		cout<<endl;
-	} */
-
-	// To prune the duplicates
-	for(unsigned int counter = 0; counter < lexicographic_min.size() - 1; counter++) {
-		for(i = (counter + 1); i < lexicographic_min.size(); i++) {
-			bool duplicate = true;
-			for(j = 0; j < lexicographic_min[i].size(); j++) {
-				for(k = 0; k < lexicographic_min[i][j].size(); k++) {
-					if(lexicographic_min[i][j][k] != lexicographic_min[counter][j][k]) {
-						duplicate = false;
-						break;
-					}
-				}
-			}
-			if(duplicate && (i != counter) && (isDuplicate(i,duplicate_entries) == false)) {
-				duplicate_entries.push_back(i);
-			}
-		}
-	}
-	for(i = 0; i < lexicographic_min.size(); i++) {
-		if(isDuplicate(i,duplicate_entries) == false) {
-			pruned_lexico_min.push_back(lexicographic_min[i]);
-		}
-	}
-
-	// To print the pruned matrices
-	/* cout<<"After Pruning:"<<endl;
-	int counter = 0;
-	for(i = 0; i < pruned_lexico_min.size(); i++) {
-			cout<<"Matrix "<<++counter<<":"<<endl;
-			for(j = 0; j < pruned_lexico_min[i].size(); j++) {
-				for(k = 0; k < pruned_lexico_min[i][j].size(); k++) {
-					cout << pruned_lexico_min[i][j][k] << " ";
-				}
-				cout << endl;
-			}
-			cout << endl;
-	} */
-
-	// To reduce the matrix
-	collection_of_matrices reduced_matrices;
-	for(i = 0; i < pruned_lexico_min.size(); i++) {
-		collection_of_rows temp_matrix;
-		for(j = 0; j < (pruned_lexico_min[i].size() - 1); j++) {
-			row temp_row;
-			for(k = 0; k < pruned_lexico_min[i][j].size(); k++) {
-				if(pruned_lexico_min[i][j][k] < (type_size + 1)) {
-					temp_row.push_back(pruned_lexico_min[i][j][k]);
-				}
-			}
-			temp_matrix.push_back(temp_row);
-		}
-		reduced_matrices.push_back(temp_matrix);
-	}
-
-	// To print reduced matrices
-	/* cout<<"After Reducing:"<<endl;
-	counter = 0;
-	for(i = 0; i < reduced_matrices.size(); i++) {
-		cout<<"Matrix "<<++counter<<":"<<endl;
-		for(j = 0; j < reduced_matrices[i].size(); j++) {
-			for(k = 0; k < reduced_matrices[i][j].size(); k++) {
-				cout << reduced_matrices[i][j][k] << " ";
-			}
-			cout << endl;
-		}
-		cout << endl;
-	} */
-
-	// To retrieve `type_matrices`
+		// To retrieve `type_matrices`
 	collection_of_matrices type_matrices = get_matrices("file5vertices.txt",6,5,4);
 
 	// To print the `type_matrices`
@@ -287,28 +110,213 @@ int main(void) {
 		matrices_in_type_matrix.push_back(temp_collection);
 	}
 
-	// To compare reduced matrices with type matrices
-	for(i = 0; i < reduced_matrices.size(); i++) {
-		for(int type = 0; type < type_matrices.size(); type++) {
-			bool is_same = true;
-			for(j = 0; j < reduced_matrices[i].size(); j++) {
-				for(k = 0; k < reduced_matrices[i][j].size(); k++) {
-					// To break the loop if element is not same
-					if(reduced_matrices[i][j][k] != type_matrices[type][j][k]) {
-						is_same = false;
-						break;
-					}
-				}
-				// To prevent checking other rows since one element is not same
-				if(is_same == false) break;
+	//int rot_sys[m_vertices][m_vertices-1] = {{2,3,4,5,6}, {1,3,4,5,6}, {1,2,4,5,6}, {1,2,3,5,6}, {1,2,6,3,4}, {1,2,5,3,4}};
+	// To read from the `input.txt` file (reads only one matrix)
+	collection_of_matrices tmp = get_matrices("input.txt",1,m_vertices,m_vertices-1);
+	int rot_sys[m_vertices][m_vertices-1];
+	memset( rot_sys, 0, m_vertices*(m_vertices-1)*sizeof(int) );
+	for(int z = 0; z < tmp.size(); z++) {
+		for(j = 0; j < tmp[z].size(); j++)
+			for(k = 0; k < tmp[z][j].size(); k++)
+				rot_sys[j][k] = tmp[z][j][k];
+
+		// To print the `rot_sys` matrix.
+		/* for(i = 0; i < m_vertices; i++) {
+			for(j = 0; j < (m_vertices-1); j++)
+				cout<<rot_sys[i][j]<<" ";
+			cout<<endl;
+		} */
+		collection_of_matrices matrices,lexicographic_min,pruned_lexico_min;
+		collection_of_collection_of_matrices classification_of_permutation;
+		int new_clock[m_vertices][m_vertices-1];
+		memset( new_clock, 0, m_vertices*(m_vertices-1)*sizeof(int) );
+
+		std::sort(v, v+m_vertices);
+
+		int final_bundle_size = factorial(m_vertices - type_size);
+
+
+		int num_bundle = 1, bundle_size = 1;
+
+		do {
+			/* for(i = 0; i < m_vertices; i++) {
+					cout<<v[i]<<" ";
 			}
-			// To push to the specific type the equivalent matrix from the pruned_lexico_min matrices.
-			if(is_same == true) {
-				matrices_in_type_matrix[type].push_back(pruned_lexico_min[i]);
-				break;
+			cout<<endl<<endl; */
+
+			dummy = search_in_array(rot_sys[v[0]-1], v[1], (m_vertices - 1));
+			for(i = 0; i < (m_vertices - 1); i++) {
+				flag = false;
+				new_clock[0][i] = 1 + search_in_array(v, rot_sys[v[0]-1][(dummy+i) % (m_vertices-1)], m_vertices);
+			}
+			for(int h = 1; h < m_vertices; h++) { // finding the relabeled vertices and rotating them such that least element comes first.
+				flag = false;
+				dummy = search_in_array(rot_sys[v[h]-1], v[0], (m_vertices-1)); // looking for the clockwise direction
+				for(i = 0; i < (m_vertices - 1); i++) {
+					flag = false;
+					new_clock[h][i] = 1 + search_in_array(v, rot_sys[v[h]-1][(dummy + i) % (m_vertices - 1)], m_vertices);
+				}
+			}
+			collection_of_rows matrix;
+			for(i = 0; i < m_vertices; i++) {
+				row temp_row;
+				for(j = 0; j < (m_vertices - 1); j++) {
+					//cout<<new_clock[i][j]<<" ";
+					temp_row.push_back(new_clock[i][j]);
+				}
+				matrix.push_back(temp_row);
+				temp_row.clear();
+			}
+			matrices.push_back(matrix);
+			matrix.clear();
+
+			if(bundle_size == final_bundle_size) {
+				bundle_size = 1;
+				++num_bundle;
+				classification_of_permutation.push_back(matrices);
+				matrices.clear();
+			} else {
+				++bundle_size;
+			}
+		}while(std::next_permutation(v,v+m_vertices));
+
+		// To print `classfication_of_permutation`
+		/* cout<<"Classification of Permutation:"<<endl;
+		for(int i=0; i<classification_of_permutation.size(); i++) {
+			cout<<"The bundle size "<<i+1<<": "<<endl;
+			for(int j=0; j<classification_of_permutation[i].size(); j++) {
+				cout<<"Matrix "<<j+1<<": "<<endl;
+				for(int k=0; k<classification_of_permutation[i][j].size(); k++) {
+					for(int l=0; l<classification_of_permutation[i][j][k].size(); l++) {
+						cout<<classification_of_permutation[i][j][k][l]<<" ";
+					}
+					cout<<endl;
+				}
+				cout<<endl;
+			}
+		} */
+
+		for(int i=0; i<classification_of_permutation.size(); i++) {
+			if(classification_of_permutation[i].size() > 1) {
+				lexicomini_of(classification_of_permutation[i], classification_of_permutation[i][0]);
+				lexicographic_min.push_back(classification_of_permutation[i][0]);
+			} else if(classification_of_permutation[i].size() == 1) {
+				lexicographic_min.push_back(classification_of_permutation[i][0]);
 			}
 		}
+
+		// To print `lexicographic_min`
+		/*
+		cout<<"Lexicographic Minimum:"<<endl;
+		for(int i=0; i<lexicographic_min.size(); i++) {
+			cout<<"Matrix "<<i+1<<": "<<endl;
+			for(int j=0; j<lexicographic_min[i].size(); j++) {
+				for(int k=0; k<lexicographic_min[i][j].size(); k++) {
+					cout<<lexicographic_min[i][j][k]<<" ";
+				}
+				cout<<endl;
+			}
+			cout<<endl;
+		} */
+
+		// To prune the duplicates
+		row duplicate_entries;
+		for(unsigned int counter = 0; counter < lexicographic_min.size() - 1; counter++) {
+			for(i = (counter + 1); i < lexicographic_min.size(); i++) {
+				bool duplicate = true;
+				for(j = 0; j < lexicographic_min[i].size(); j++) {
+					for(k = 0; k < lexicographic_min[i][j].size(); k++) {
+						if(lexicographic_min[i][j][k] != lexicographic_min[counter][j][k]) {
+							duplicate = false;
+							break;
+						}
+					}
+				}
+				if(duplicate && (i != counter) && (isDuplicate(i,duplicate_entries) == false)) {
+					duplicate_entries.push_back(i);
+				}
+			}
+		}
+		for(i = 0; i < lexicographic_min.size(); i++) {
+			if(isDuplicate(i,duplicate_entries) == false) {
+				pruned_lexico_min.push_back(lexicographic_min[i]);
+			}
+		}
+
+		// To print the pruned matrices
+		/* cout<<"After Pruning:"<<endl;
+		int counter = 0;
+		for(i = 0; i < pruned_lexico_min.size(); i++) {
+				cout<<"Matrix "<<++counter<<":"<<endl;
+				for(j = 0; j < pruned_lexico_min[i].size(); j++) {
+					for(k = 0; k < pruned_lexico_min[i][j].size(); k++) {
+						cout << pruned_lexico_min[i][j][k] << " ";
+					}
+					cout << endl;
+				}
+				cout << endl;
+		} */
+
+		// To reduce the matrix
+		collection_of_matrices reduced_matrices;
+		for(i = 0; i < pruned_lexico_min.size(); i++) {
+			collection_of_rows temp_matrix;
+			for(j = 0; j < (pruned_lexico_min[i].size() - 1); j++) {
+				row temp_row;
+				for(k = 0; k < pruned_lexico_min[i][j].size(); k++) {
+					if(pruned_lexico_min[i][j][k] < (type_size + 1)) {
+						temp_row.push_back(pruned_lexico_min[i][j][k]);
+					}
+				}
+				temp_matrix.push_back(temp_row);
+			}
+			reduced_matrices.push_back(temp_matrix);
+		}
+
+		// To print reduced matrices
+		/* cout<<"After Reducing:"<<endl;
+		counter = 0;
+		for(i = 0; i < reduced_matrices.size(); i++) {
+			cout<<"Matrix "<<++counter<<":"<<endl;
+			for(j = 0; j < reduced_matrices[i].size(); j++) {
+				for(k = 0; k < reduced_matrices[i][j].size(); k++) {
+					cout << reduced_matrices[i][j][k] << " ";
+				}
+				cout << endl;
+			}
+			cout << endl;
+		} */
+
+		// To compare reduced matrices with type matrices
+		for(i = 0; i < reduced_matrices.size(); i++) {
+			for(int type = 0; type < type_matrices.size(); type++) {
+				bool is_same = true;
+				for(j = 0; j < reduced_matrices[i].size(); j++) {
+					for(k = 0; k < reduced_matrices[i][j].size(); k++) {
+						// To break the loop if element is not same
+						if(reduced_matrices[i][j][k] != type_matrices[type][j][k]) {
+							is_same = false;
+							break;
+						}
+					}
+					// To prevent checking other rows since one element is not same
+					if(is_same == false) break;
+				}
+				// To push to the specific type the equivalent matrix from the pruned_lexico_min matrices.
+				if(is_same == true) {
+					matrices_in_type_matrix[type].push_back(pruned_lexico_min[i]);
+					break;
+				}
+			}
+		}
+		memset( rot_sys, 0, m_vertices*(m_vertices-1)*sizeof(int) );
+		classification_of_permutation.clear();
+		lexicographic_min.clear();
+		duplicate_entries.clear();
+		pruned_lexico_min.clear();
+		reduced_matrices.clear();
 	}
+
 	// To write the all matrices present in type matrices as separate file in `output` folder
   for(i = 0; i < matrices_in_type_matrix.size(); i++) {
   	ofstream outfile;
